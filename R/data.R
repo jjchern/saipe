c5122a945fcdbc75078c39d2236bfd61b1470f1b
jjchern <jiajia.chern@gmail.com>
2016-05-19 10:58:17 -0500

    Add simple description

--- R/data.R
+++ R/data.R
@@ -1,11 +1,15 @@
 #' @title SAIPE Variable Desciptions
+#' @description Use \code{saipe::saipe_vars}.
 "saipe_vars"
 
 #' @title US Poverty Count and Rate Estimates
+#' @description Use \code{saipe::saipe_us}.
 "saipe_us"
 
 #' @title State-Level Poverty Count and Rate Estimates
+#' @description Use \code{saipe::saipe_state}.
 "saipe_state"
 
 #' @title County-Level Poverty Count and Rate Estimates
+#' @description Use \code{saipe::saipe_county}.
 "saipe_county"
