7898e2df83a42e7c3f3ceb89d9b65f27c673e238
jjchern <jiajia.chern@gmail.com>
2019-04-22 22:19:31 -0500

    Add 2017 data and standardize col names

--- R/data.R
+++ R/data.R
@@ -1,15 +1,72 @@
-#' @title SAIPE Variable Desciptions
-#' @description Use \code{saipe::saipe_vars}.
-"saipe_vars"
 
 #' @title US Poverty Count and Rate Estimates
-#' @description Use \code{saipe::saipe_us}.
+#' @description Use `saipe::saipe_us`.
+#' @format A data frame with the following variables:
+#' - `county`: County FIPS Code
+#' - `countyfip`: State+County FIPS Code
+#' - `name`: State or County Name
+#' - `med_inc`: Median Household Income Estimate
+#' - `pov_cnt_0_17`: Ages 0-17 in Poverty, Count Estimate
+#' - `pov_cnt_0_4`: Ages 0-4 in Poverty, Count Estimate
+#' - `pov_cnt_fam_5_17`: Ages 5-17 in Families in Poverty, Count Estimate
+#' - `pov_cnt_all`: All ages in Poverty, Count Estimate
+#' - `pov_rate_0_17`: Ages 0-17 in Poverty, Rate Estimate
+#' - `pov_rate_0_4`: Ages 0-4 in Poverty, Rate Estimate
+#' - `pov_rate_all`: All ages in Poverty, Rate Estimate
+#' - `pov_univ_0_17`: Ages 0-17 in Poverty Universe
+#' - `pov_univ_0_4`: Ages 0-4 in Poverty Universe
+#' - `pov_univ_fam_5_17`: Ages 5-17r in Poverty Universe
+#' - `pov_univ_all`: All Ages in Poverty Universe
+#' - `abbr`: Two-letter State Postal abbreviation
+#' - `statefip`: FIPS State Code
+#' - `sum_lvl`: Summary Level
+#' - `year`: time
 "saipe_us"
 
 #' @title State-Level Poverty Count and Rate Estimates
-#' @description Use \code{saipe::saipe_state}.
+#' @description Use `saipe::saipe_state`.
+#' @format A data frame with the following variables:
+#' - `county`: County FIPS Code
+#' - `countyfip`: State+County FIPS Code
+#' - `name`: State or County Name
+#' - `med_inc`: Median Household Income Estimate
+#' - `pov_cnt_0_17`: Ages 0-17 in Poverty, Count Estimate
+#' - `pov_cnt_0_4`: Ages 0-4 in Poverty, Count Estimate
+#' - `pov_cnt_fam_5_17`: Ages 5-17 in Families in Poverty, Count Estimate
+#' - `pov_cnt_all`: All ages in Poverty, Count Estimate
+#' - `pov_rate_0_17`: Ages 0-17 in Poverty, Rate Estimate
+#' - `pov_rate_0_4`: Ages 0-4 in Poverty, Rate Estimate
+#' - `pov_rate_all`: All ages in Poverty, Rate Estimate
+#' - `pov_univ_0_17`: Ages 0-17 in Poverty Universe
+#' - `pov_univ_0_4`: Ages 0-4 in Poverty Universe
+#' - `pov_univ_fam_5_17`: Ages 5-17r in Poverty Universe
+#' - `pov_univ_all`: All Ages in Poverty Universe
+#' - `abbr`: Two-letter State Postal abbreviation
+#' - `statefip`: FIPS State Code
+#' - `sum_lvl`: Summary Level
+#' - `year`: time
 "saipe_state"
 
 #' @title County-Level Poverty Count and Rate Estimates
-#' @description Use \code{saipe::saipe_county}.
+#' @description Use `saipe::saipe_county`.
+#' @format A data frame with the following variables:
+#' - `county`: County FIPS Code
+#' - `countyfip`: State+County FIPS Code
+#' - `name`: State or County Name
+#' - `med_inc`: Median Household Income Estimate
+#' - `pov_cnt_0_17`: Ages 0-17 in Poverty, Count Estimate
+#' - `pov_cnt_0_4`: Ages 0-4 in Poverty, Count Estimate
+#' - `pov_cnt_fam_5_17`: Ages 5-17 in Families in Poverty, Count Estimate
+#' - `pov_cnt_all`: All ages in Poverty, Count Estimate
+#' - `pov_rate_0_17`: Ages 0-17 in Poverty, Rate Estimate
+#' - `pov_rate_0_4`: Ages 0-4 in Poverty, Rate Estimate
+#' - `pov_rate_all`: All ages in Poverty, Rate Estimate
+#' - `pov_univ_0_17`: Ages 0-17 in Poverty Universe
+#' - `pov_univ_0_4`: Ages 0-4 in Poverty Universe
+#' - `pov_univ_fam_5_17`: Ages 5-17r in Poverty Universe
+#' - `pov_univ_all`: All Ages in Poverty Universe
+#' - `abbr`: Two-letter State Postal abbreviation
+#' - `statefip`: FIPS State Code
+#' - `sum_lvl`: Summary Level
+#' - `year`: time
 "saipe_county"
--- data-raw/prep.R
+++ data-raw/prep.R
@@ -1,24 +1,137 @@
 
 # install.packages("devtools")
-# devtools::install_github("jjchern/saipeAPI@v0.0.2")
-library(dplyr, warn.conflicts = FALSE)
+# devtools::install_github("jjchern/saipeAPI@v0.0.3")
+library(tidyverse)
 
 # SAIPE variable labels ---------------------------------------------------
 
-saipe_vars = saipeAPI::saipe_vars %>% filter(Concept %>% stringr::str_detect("Estimate|Geo"))
-devtools::use_data(saipe_vars, overwrite = TRUE)
+saipeAPI::saipe_vars %>%
+        filter(str_detect(Concept, "Estimate|Geo")) %>%
+        select(Name, Label) %>%
+        print() -> saipe_vars
 
 # US Poverty, Count and Rate Estimates ------------------------------------
 
-saipe_us = saipeAPI::saipe_us(year = saipeAPI::saipe_years, var = saipe_vars$Name)
-devtools::use_data(saipe_us, overwrite = TRUE)
+saipeAPI::saipe_us(year = saipeAPI::saipe_years,
+                   var = saipe_vars$Name) %>%
+        print() -> saipe_us_raw
+
+saipe_vars %>%
+        mutate(Name = tolower(Name))
+
+saipe_us_raw %>%
+        docxtractr::mcga() %>%
+        select(-county, -geoid, -us, -state) %>%
+        select(name, abbr = stabrev, year = time,
+               sum_lvl = sumlev,
+               med_inc = saemhi_pt,
+               pov_cnt_0_17      = saepov0_17_pt,
+               pov_cnt_0_4       = saepov0_4_pt,
+               pov_cnt_fam_5_17  = saepov5_17r_pt,
+               pov_cnt_all       = saepovall_pt,
+               pov_rate_0_17     = saepovrt0_17_pt,
+               pov_rate_0_4      = saepovrt0_4_pt,
+               pov_rate_all      = saepovrtall_pt,
+               pov_univ_0_17     = saepovu_0_17,
+               pov_univ_0_4      = saepovu_0_4,
+               pov_univ_fam_5_17 = saepovu_5_17r,
+               pov_univ_all      = saepovu_all,
+               everything()) %>%
+        print() -> saipe_us
+
+usethis::use_data(saipe_us, overwrite = TRUE)
 
 # State Poverty, Count and Rate Estimates ---------------------------------
 
-saipe_state = saipeAPI::saipe_state(year = saipeAPI::saipe_years, var = saipe_vars$Name)
-devtools::use_data(saipe_state, overwrite = TRUE)
+saipeAPI::saipe_state(year = saipeAPI::saipe_years,
+                      var = saipe_vars$Name) %>%
+        print() -> saipe_state_raw
+
+saipe_state_raw %>%
+        docxtractr::mcga() %>%
+        select(-county, -state_1, -geoid) %>%
+        select(name,
+               abbr = stabrev,
+               statefip = state,
+               year = time,
+               sum_lvl = sumlev,
+               med_inc = saemhi_pt,
+               pov_cnt_0_17      = saepov0_17_pt,
+               pov_cnt_0_4       = saepov0_4_pt,
+               pov_cnt_fam_5_17  = saepov5_17r_pt,
+               pov_cnt_all       = saepovall_pt,
+               pov_rate_0_17     = saepovrt0_17_pt,
+               pov_rate_0_4      = saepovrt0_4_pt,
+               pov_rate_all      = saepovrtall_pt,
+               pov_univ_0_17     = saepovu_0_17,
+               pov_univ_0_4      = saepovu_0_4,
+               pov_univ_fam_5_17 = saepovu_5_17r,
+               pov_univ_all      = saepovu_all,
+               everything()) %>%
+        print() -> saipe_state
+
+saipe_state %>% count(year) %>% print(n = 25)
+
+usethis::use_data(saipe_state, overwrite = TRUE)
 
 # County Poverty, Count and Rate Estimates --------------------------------
 
-saipe_county = saipeAPI::saipe_county(year = saipeAPI::saipe_years, var = saipe_vars$Name)
-devtools::use_data(saipe_county, overwrite = TRUE)
+saipeAPI::saipe_county(year = saipeAPI::saipe_years,
+                       var = saipe_vars$Name) %>%
+        print() -> saipe_county_raw
+
+saipe_county_raw %>%
+        glimpse()
+
+saipe_county_raw %>%
+        docxtractr::mcga() %>%
+        select(-county, -state_1, -county_1) %>%
+        select(name,
+               abbr = stabrev,
+               statefip = state,
+               countyfip = geoid,
+               year = time,
+               sum_lvl = sumlev,
+               med_inc = saemhi_pt,
+               pov_cnt_0_17      = saepov0_17_pt,
+               pov_cnt_0_4       = saepov0_4_pt,
+               pov_cnt_fam_5_17  = saepov5_17r_pt,
+               pov_cnt_all       = saepovall_pt,
+               pov_rate_0_17     = saepovrt0_17_pt,
+               pov_rate_0_4      = saepovrt0_4_pt,
+               pov_rate_all      = saepovrtall_pt,
+               pov_univ_0_17     = saepovu_0_17,
+               pov_univ_0_4      = saepovu_0_4,
+               pov_univ_fam_5_17 = saepovu_5_17r,
+               pov_univ_all      = saepovu_all,
+               everything()) %>%
+        print() -> saipe_county
+
+usethis::use_data(saipe_county, overwrite = TRUE)
+
+# Prepare variable labels -------------------------------------------------
+
+saipe_vars %>%
+        mutate(Name = tolower(Name)) %>%
+        pivot_wider(names_from = Name, values_from = Label) %>%
+        rename(abbr = stabrev,
+               statefip = state,
+               countyfip = geoid,
+               sum_lvl = sumlev,
+               med_inc = saemhi_pt,
+               pov_cnt_0_17      = saepov0_17_pt,
+               pov_cnt_0_4       = saepov0_4_pt,
+               pov_cnt_fam_5_17  = saepov5_17r_pt,
+               pov_cnt_all       = saepovall_pt,
+               pov_rate_0_17     = saepovrt0_17_pt,
+               pov_rate_0_4      = saepovrt0_4_pt,
+               pov_rate_all      = saepovrtall_pt,
+               pov_univ_0_17     = saepovu_0_17,
+               pov_univ_0_4      = saepovu_0_4,
+               pov_univ_fam_5_17 = saepovu_5_17r,
+               pov_univ_all      = saepovu_all) %>%
+        mutate(year = "time") %>%
+        pivot_longer(cols = everything(),
+                     names_to = "name", values_to = "label") %>%
+        transmute(lbl = str_glue("- `{name}`: {label}")) %>%
+        pull(lbl)
